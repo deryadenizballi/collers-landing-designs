f7beb096563723699145ae33fe9dce48aa3b418c
Derya Deniz Ballı <dderyaaballi@gmail.com>
2024-04-19 23:13:15 +0300

    Completed footer section of website

---
+++ assets/custom.js
@@ -0,0 +1,11 @@
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 4.5,
+    spaceBetween: 24,
+    loop:true,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+  });
+
+  console.log("deneme")
